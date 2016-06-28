0dfa5376cbf5e6e4d2b17207243f416760630fc5
Franco Montiel <francomontiel@hotmail.com>
2016-06-28 14:25:15 -0400

    Minor improvements on ncurses window, still array not working

--- ncurses.c
+++ ncurses.c
@@ -1,6 +1,8 @@
+#include <stdio.h>
 #include <ncurses.h>
 
 char nameTemp[160];
+char * array[50], c, pos, cap;
 
 void drawLogo() {
 	mvprintw(8,8,"        /\\            \\            / /        /       #######");
@@ -10,11 +12,25 @@ void drawLogo() {
 	mvprintw(12,8,"    /        \\/__//__/    \\/  \\/ /  / /_/ /_ __/   #######   ");
 	mvprintw(13,8,"             /   /                                           ");
 	mvprintw(14,8,"____________/   /____________________________________________");
-	mvprintw(15,8,"_______________PRESS ANY KEY TO CHAT...NOW :v________________");
+	mvprintw(15,8,"__________PRESS ANY KEY TO CHAT LIKE A REAL DEV :v___________");
+}
+ 
+/*
+void scrollBuffer() {
+	for ( c = position - 1 ; c < n - 1 ; c++ )
+    	array[c] = array[c+1]; 
+}
+*/
+void printBuffer(char* msg) {
+	array[pos] = msg;
+	//for( c = 0 ; c < 9 ; c++ )
+	mvprintw( 4, 3, array[0]);
+	mvprintw( 5, 3, array[1]);
+	mvprintw( 6, 3, array[2]);
 }
 
-int main()
-{
+int main() {
+
 	initscr();
 	raw();
 	start_color();
@@ -24,29 +40,31 @@ int main()
 	attron(COLOR_PAIR(1));
 	drawLogo();
 	attroff(COLOR_PAIR(1));
-	
 	attron(COLOR_PAIR(2));
 	getch(); //Press any key 2 advance
-	wborder(local_win, '{', '}', '^','v','*','*','*','*'); //Draw interface
-	wbkgd(local_win, COLOR_PAIR(2));
-	mvprintw(0, 14, "== AppWhats 2 TM - Just plain chatting, literally =="); //Draws title
-	mvprintw(18, 1, "==============================================================================");
-	attroff(COLOR_PAIR(2));
-	
-	attron(COLOR_PAIR(1)); //Black background for user writing.
-	mvprintw(19, 1, "                                                                              ");
-	mvprintw(20, 1, "                                                                              ");
-	mvprintw(21, 1, "                                                                              ");
-	mvprintw(22, 1, "                                                                              ");
-	attroff(COLOR_PAIR(1));
-	wrefresh(local_win);
+		wborder(local_win, '{', '}', '^','v','*','*','*','*'); //Draw interface
+		wbkgd(local_win, COLOR_PAIR(2));
+		mvprintw(0, 14, "== AppWhats 2 TM - Just plain chatting, literally =="); //Draws title
+		mvprintw(18, 1, "==============================================================================");
+		attroff(COLOR_PAIR(2));
 
-	move(19, 3);
-    getstr(nameTemp); //Start chatting
+	pos = 0;
+	for (; ;)	{
+		attron(COLOR_PAIR(1)); //Black background for user writing.
+		mvprintw(19, 1, "                                                                              ");
+		mvprintw(20, 1, "                                                                              ");
+		mvprintw(21, 1, "                                                                              ");
+		mvprintw(22, 1, "                                                                              ");
+		attroff(COLOR_PAIR(1));
+		wrefresh(local_win);
 
-	endwin();
+		move(19, 3);
+	    getstr(nameTemp); //Start chatting
+	    printBuffer(nameTemp);
+		pos = pos + 1;
 
-	printf("%s\n", nameTemp); //Do whatever you want with the str.
-
+//	    if (getch() == ']')
+//	    	endwin();
+	}
 	return 0;
 }
